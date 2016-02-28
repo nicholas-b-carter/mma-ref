89fe722fff152245901fc51ce4bb0fca98d10518
Nicholas Carter <Nick@Nicholass-MacBook-Pro.local>
2016-02-28 17:05:27 -0500

    Initial Commit

---
+++ app/src/controllers/settingsController.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name MmaRef.controller:SettingsController
+ * @description
+ * # SettingsController
+ */
+module.exports = [
+    '$scope',
+
+    function( $scope )
+    {
+        // do something with $scope
+    }
+];
